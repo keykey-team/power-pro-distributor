00ff7ef03559a445a62c6ce3c5d2325a15d7ed92
gordienko <keykeyteams@gmail.com>
2026-03-12 16:42:57 +0300

    add config

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,53 @@
-
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 
   images: {
-    domains: [
-      "i.postimg.cc",
-      "localhost:8080",
-      "192.168.1.126",
-      "sales-box-photos.s3-eu-central-1.amazonaws.com",
-      "yourcdn.com",
-      "fitwin-powerpro.com",
-      "d3og57k1dk4307.cloudfront.net",
-      "localhost",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8080',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '192.168.1.126',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sales-box-photos.s3-eu-central-1.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yourcdn.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fitwin-powerpro.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd3og57k1dk4307.cloudfront.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
